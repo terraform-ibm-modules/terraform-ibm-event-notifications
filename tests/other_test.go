3ad84e38c22fc03a080e556f5a6ba41b6bea7434
Akash Kumar <akash.kumar@ibm.com>
2023-11-06 23:30:00 +0530

    moving back basic test to other test

---
+++ tests/other_test.go
@@ -0,0 +1,20 @@
+// Tests in this file are NOT run in the PR pipeline. They are run in the continuous testing pipeline along with the ones in pr_test.go
+package test
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+const basicExampleDir = "examples/basic"
+
+func TestRunBasicExample(t *testing.T) {
+	t.Parallel()
+
+	options := setupOptions(t, "event-notification-basic", basicExampleDir)
+
+	output, err := options.RunTestConsistency()
+	assert.Nil(t, err, "This should not have errored")
+	assert.NotNil(t, output, "Expected some output")
+}
