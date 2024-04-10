df1f0b3e9544c1603153be9b2fd92c0627048bdf
akocbek@ie.ibm.com <akocbek@ie.ibm.com>
2024-04-10 11:37:54 +0100

    feat: update DA inputs

--- solutions/standard/main.tf
+++ solutions/standard/main.tf
@@ -26,7 +26,7 @@ module "kms" {
   providers = {
     ibm = ibm.kms
   }
-  count                       = local.existing_kms_root_key_id != null ? 0 : 1 # no need to create any KMS resources if passing an existing key
+  count                       = var.existing_kms_root_key_crn != null ? 0 : 1 # no need to create any KMS resources if passing an existing key
   source                      = "terraform-ibm-modules/kms-all-inclusive/ibm"
   version                     = "4.8.4"
   resource_group_id           = null # rg only needed if creating KP instance
