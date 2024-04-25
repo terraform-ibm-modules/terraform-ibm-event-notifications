7df0ce8a051647cb158a67fd4e470883c7a3eaac
Nora Hooz <Nora.Hooz@ibm.com>
2024-04-25 12:38:00 +0200

    first round of content updates

--- variables.tf
+++ variables.tf
@@ -9,47 +9,47 @@ variable "resource_group_id" {
 
 variable "name" {
   type        = string
-  description = "The name to give the IBM Event Notification instance created by this module."
+  description = "The name of the Event Notifications instance that is created by this module."
 }
 
 variable "plan" {
   type        = string
-  description = "Plan for the event notification instance : lite or standard"
+  description = "The pricing plan of the Event Notifications instance. The following pricing plans are available: `Lite`, `Standard`"
   default     = "standard"
   validation {
     condition     = contains(["lite", "standard"], var.plan)
-    error_message = "The specified plan is not a valid selection! Supported plans are: lite or standard"
+    error_message = "The specified pricing plan is not available! The following plans are supported: `Lite`, `Standard`"
   }
 }
 
 variable "tags" {
   type        = list(string)
-  description = "Optional list of tags to be added to the Event Notification instance"
+  description = "The optional list of tags to add to the Event Notifications instance."
   default     = []
 }
 
 variable "region" {
   type        = string
-  description = "IBM Cloud region where event notification will be created, supported regions are: us-south (Dallas), eu-gb (London), eu-de (Frankfurt), au-syd (Sydney), eu-es (Madrid)"
+  description = "The IBM Cloud region where the Event Notifications resource is created. The following regions are supported: `us-south` (Dallas), `eu-gb` (London), `eu-de` (Frankfurt), `au-syd` (Sydney), `eu-es` (Madrid)"
   default     = "us-south"
   validation {
     condition     = contains(["us-south", "eu-gb", "eu-de", "au-syd", "eu-es", "eu-fr2"], var.region)
-    error_message = "The specified region is not valid, supported regions are: us-south (Dallas), eu-gb (London), eu-de (Frankfurt), au-syd (Sydney), eu-es (Madrid), eu-fr2 (BNPP)"
+    error_message = "The specified region is not supported! The following regions are supported: `us-south` (Dallas), `eu-gb` (London), `eu-de` (Frankfurt), `au-syd` (Sydney), `eu-es` (Madrid), `eu-fr2` (BNPP)"
   }
 }
 variable "kms_endpoint_url" {
-  description = "The KMS endpoint URL to use when configuring KMS encryption. HPCS endpoint URL format- https://api.private.<REGION>.hs-crypto.cloud.ibm.com:<port> and KP endpoint URL format- https://<REGION>.kms.cloud.ibm.com"
+  description = "The URL of the key management service (KMS) endpoint to use when configuring KMS encryption. The Hyper Protect Crypto Services (HPCS) endpoint URL format is https://api.private.<REGION>.hs-crypto.cloud.ibm.com:<port> and the Key Protect (KP) endpoint URL format is https://<REGION>.kms.cloud.ibm.com"
   type        = string
   default     = null
 }
 
 variable "service_endpoints" {
   type        = string
-  description = "Specify whether you want to enable the public or both public and private service endpoints. Supported values are 'public' or 'public-and-private'."
+  description = "Specify whether you want to enable public, or both public and private service endpoints. The following values are supported: `public`, `public-and-private`"
   default     = "public-and-private"
   validation {
     condition     = contains(["public", "public-and-private"], var.service_endpoints)
-    error_message = "The specified service endpoint is not a valid selection! Supported options are: public or public-and-private."
+    error_message = "The specified service endpoint is not supported! The following endpoint options are supported: `public`, `public-and-private`"
   }
 }
 
@@ -64,42 +64,42 @@ variable "cbr_rules" {
     }))) }))
     enforcement_mode = string
   }))
-  description = "(Optional, list) List of CBR rules to create"
+  description = "(Optional, list) The list of context-based restrictions (CBR) rules to create."
   default     = []
 }
 
 variable "skip_iam_authorization_policy" {
   type        = bool
-  description = "Set to true to skip the creation of an IAM authorization policy that permits all Event Notification instances in the resource group to read the encryption key from the KMS instance. No policy is created if var.kms_encryption_enabled is set to false."
+  description = "Set this variable value to `true` to skip the creation of an IAM authorization policy that permits all Event Notifications instances in the resource group to read the encryption key from the key management service (KMS) instance. No policy is created if `var.kms_encryption_enabled` variable is set to `false`."
   default     = false
 }
 
 variable "kms_encryption_enabled" {
   type        = bool
-  description = "Set this to true to control the encryption keys used to encrypt the data that you store in Event Notification. If set to false, the data is encrypted by using randomly generated keys. For more info on Managing Encryption, see https://cloud.ibm.com/docs/event-notifications?topic=event-notifications-en-managing-encryption"
+  description = "Set this variable value to `true` to control the encryption keys that are used to encrypt the data that you store in the Event Notifications instance. If this value is set to `false`, the data is encrypted by using randomly generated keys. For more information, see Managing encryption https://cloud.ibm.com/docs/event-notifications?topic=event-notifications-en-managing-encryption."
   default     = false
 }
 
 variable "existing_kms_instance_crn" {
-  description = "The CRN of the Hyper Protect Crypto Services or Key Protect instance. Required only if var.kms_encryption_enabled is set to true"
+  description = "The Cloud Resource Name (CRN) of the Hyper Protect Crypto Services or Key Protect instance. This variable is required only if `var.kms_encryption_enabled` is set to `true`."
   type        = string
   default     = null
 }
 
 variable "root_key_id" {
   type        = string
-  description = "The Key ID of a root key, existing in the KMS instance passed in var.existing_kms_instance_crn, which will be used to encrypt the data encryption keys (DEKs) which are then used to encrypt the data. Required if var.kms_encryption_enabled is set to true."
+  description = "The key ID of a root key, existing in the key management service (KMS) instance passed in `var.existing_kms_instance_crn`, which will be used to encrypt the data encryption keys (DEKs) which are then used to encrypt the data. This variable is required if `var.kms_encryption_enabled` is set to `true`."
   default     = null
 }
 
 
 variable "service_credential_names" {
-  description = "Map of name, role for service credentials that you want to create for the event notification"
+  description = "The mapping of names and roles for service credentials that you want to create for the Event Notifications instance."
   type        = map(string)
   default     = {}
 
   validation {
     condition     = alltrue([for name, role in var.service_credential_names : contains(["Manager", "Writer", "Reader", "Event Source Manager", "Channel Editor", "Event Notification Publisher", "Status Reporter", "Device Manager", "Email Sender", "Custom Email Status Reporter"], role)])
-    error_message = "Valid values for service credential roles are 'Manager', 'Writer', 'Reader', 'Event Source Manager', 'Channel Editor', 'Event Notification Publisher', 'Status Reporter', 'Device Manager', 'Email Sender', 'Custom Email Status Reporter'"
+    error_message = "The specified service credential role is not valid! The following values are valid for service credential roles: 'Manager', 'Writer', 'Reader', 'Event Source Manager', 'Channel Editor', 'Event Notification Publisher', 'Status Reporter', 'Device Manager', 'Email Sender', 'Custom Email Status Reporter'"
   }
 }
