66512d2dcd4cf8601bb1e3d451f2a4775a9d3411
Akash Kumar <Ak-sky@users.noreply.github.com>
2024-06-27 12:28:50 +0530

    fix: updated CBR module to `1.23.0`<br>- updated required ibm provider version to `>= 1.65.0, <2.0.0` (#236)
    * fix: update required ibm provider version to >= 1.65.0
    
    * bumped the CBR module version

--- examples/fscloud/main.tf
+++ examples/fscloud/main.tf
@@ -40,7 +40,7 @@ resource "ibm_is_subnet" "testacc_subnet" {
 
 module "cbr_zone" {
   source           = "terraform-ibm-modules/cbr/ibm//modules/cbr-zone-module"
-  version          = "1.22.2"
+  version          = "1.23.0"
   name             = "${var.prefix}-VPC-network-zone"
   zone_description = "CBR Network zone representing VPC"
   account_id       = data.ibm_iam_account_settings.iam_account_settings.account_id
