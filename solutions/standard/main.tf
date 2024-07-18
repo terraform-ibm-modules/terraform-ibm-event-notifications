a8a4ad428c84b2358767144d442f01b8e3d9f5f9
Akash Kumar <akash.kumar@ibm.com>
2024-07-18 15:58:39 +0530

    modified DA

--- solutions/standard/main.tf
+++ solutions/standard/main.tf
@@ -79,7 +79,6 @@ module "kms" {
 locals {
   # tflint-ignore: terraform_unused_declarations
   validate_cos_regions        = var.cos_bucket_region != null && var.cross_region_location != null ? tobool("Cannot provide values for var.cos_bucket_region and var.cross_region_location") : true
-  cos_instance_guid           = var.existing_cos_instance_crn != null ? var.existing_cos_instance_crn : module.cos[0].cos_instance_guid
   cos_bucket_name             = var.existing_cos_bucket_name != null ? var.existing_cos_bucket_name : (var.prefix != null ? "${var.prefix}-${var.cos_bucket_name}" : var.cos_bucket_name)
   cos_bucket_name_with_suffix = var.existing_cos_bucket_name != null ? var.existing_cos_bucket_name : module.cos[0].bucket_name
   cos_bucket_region           = var.cos_bucket_region != null ? var.cos_bucket_region : var.cross_region_location != null ? null : var.region
@@ -141,7 +140,7 @@ module "event_notifications" {
   # COS Related
   cos_integration_enabled = true
   cos_bucket_name         = local.cos_bucket_name_with_suffix
-  cos_instance_id         = local.cos_instance_guid
+  cos_instance_id         = var.existing_cos_instance_crn != null ? var.existing_cos_instance_crn : module.cos[0].cos_instance_crn
   skip_en_cos_auth_policy = var.skip_en_cos_auth_policy
   cos_endpoint            = local.cos_endpoint
 }
