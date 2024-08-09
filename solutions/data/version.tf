dc77ca3107aa7fb5d2223e77e29b4219a30db6f4
Steve Peggs <peggs@uk.ibm.com>
2024-08-09 18:43:13 +0100

    feat: event notifications as a data source

---
+++ solutions/data/version.tf
@@ -0,0 +1,10 @@
+terraform {
+  required_version = ">= 1.3.0"
+  # Lock DA into an exact provider version - renovate automation will keep it updated
+  required_providers {
+    ibm = {
+      source  = "IBM-Cloud/ibm"
+      version = "1.67.1"
+    }
+  }
+}
