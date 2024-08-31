db3718e9c547a905b0617a4565aaafcabb4227b8
Terraform IBM Modules Operations <Terraform.IBM.Modules.Operations@ibm.com>
2024-08-31 19:35:45 +0000

    fix(deps): update terraform terraform-ibm-modules/cos/ibm to v8.11.2

--- examples/complete/main.tf
+++ examples/complete/main.tf
@@ -46,7 +46,7 @@ locals {
 
 module "cos" {
   source                 = "terraform-ibm-modules/cos/ibm"
-  version                = "8.10.7"
+  version                = "8.11.2"
   resource_group_id      = module.resource_group.resource_group_id
   region                 = var.region
   cos_instance_name      = "${var.prefix}-cos"
