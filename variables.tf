5baa6dc815e32cc33d84af5a42600db5782da710
Jordan-Williams2 <Jordan.Williams2@ibm.com>
2024-11-06 09:39:27 +0000

    fix: docs

--- variables.tf
+++ variables.tf
@@ -56,7 +56,7 @@ variable "region" {
   }
 }
 variable "kms_endpoint_url" {
-  description = "The URL of the KMS endpoint to use when configuring KMS encryption. The Hyper Protect Crypto Services endpoint URL format can be found at https://cloud.ibm.com/docs/hs-crypto?topic=hs-crypto-regions#new-service-endpoints, and the Key Protect endpoint URL format is `https://<REGION>.kms.cloud.ibm.com`."
+  description = "The URL of the KMS endpoint to use when configuring KMS encryption. The Hyper Protect Crypto Services endpoint URL format can be found at https://cloud.ibm.com/docs/hs-crypto?topic=hs-crypto-regions#new-service-endpoints, and the Key Protect endpoint URL format can be found here https://cloud.ibm.com/docs/key-protect?topic=key-protect-regions#service-endpoints."
   type        = string
   default     = null
 }
