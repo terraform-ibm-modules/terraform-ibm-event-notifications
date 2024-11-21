b91d89e2036a0196fe8cc4a46e26f62efbcc78c7
Aditya-ranjan-16 <aditya16ranjan@gmail.com>
2024-11-22 05:21:13 +0530

    feat:exposed visibility value for the provider in the DA

--- solutions/standard/provider.tf
+++ solutions/standard/provider.tf
@@ -1,10 +1,12 @@
 provider "ibm" {
   ibmcloud_api_key = var.ibmcloud_api_key
   region           = var.region
+  visibility       = var.provider_visibility
 }
 
 provider "ibm" {
   alias            = "kms"
   ibmcloud_api_key = var.ibmcloud_kms_api_key != null ? var.ibmcloud_kms_api_key : var.ibmcloud_api_key
   region           = local.kms_region
+  visibility       = var.provider_visibility
 }
