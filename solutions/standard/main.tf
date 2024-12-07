6dca5e7b0448c5289de6e4b211b6f3a581887a39
Terraform IBM Modules Operations <106112202+terraform-ibm-modules-ops@users.noreply.github.com>
2024-12-07 14:08:07 -0500

    fix(deps): update terraform terraform-ibm-modules/cos/ibm to v8.15.10 (#346)

--- solutions/standard/main.tf
+++ solutions/standard/main.tf
@@ -247,7 +247,7 @@ locals {
 module "cos" {
   count                               = local.create_cos_bucket ? 1 : 0
   source                              = "terraform-ibm-modules/cos/ibm"
-  version                             = "8.15.9"
+  version                             = "8.15.10"
   create_cos_instance                 = var.existing_cos_instance_crn == null ? true : false
   create_cos_bucket                   = local.create_cos_bucket
   existing_cos_instance_id            = var.existing_cos_instance_crn
