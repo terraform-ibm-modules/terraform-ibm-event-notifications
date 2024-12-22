ac3374a6c9399030aa0f4799ac0f7ad31f7474a0
Terraform IBM Modules Operations <Terraform.IBM.Modules.Operations@ibm.com>
2024-12-22 21:04:22 +0000

    fix(deps): update terraform-module

--- examples/complete/main.tf
+++ examples/complete/main.tf
@@ -45,7 +45,7 @@ locals {
 
 module "cos" {
   source                 = "terraform-ibm-modules/cos/ibm"
-  version                = "8.15.15"
+  version                = "8.15.16"
   resource_group_id      = module.resource_group.resource_group_id
   region                 = var.region
   cos_instance_name      = "${var.prefix}-cos"
