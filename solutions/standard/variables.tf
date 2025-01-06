4078be5148010484a85a84831fc8e82e5071eba8
kierra.searle@ibm.com <kierra.searle@ibm.com>
2025-01-06 16:15:57 -0500

    fix: prefix validation update

--- solutions/standard/variables.tf
+++ solutions/standard/variables.tf
@@ -46,8 +46,14 @@ variable "prefix" {
   description = "(Optional) Prefix to add to all resources created by this solution."
   default     = null
   validation {
-    error_message = "Prefix must begin with a lowercase letter and contain only lowercase letters, numbers, and - characters. Prefixes must end with a lowercase letter or number and be 16 or fewer characters."
-    condition     = can(regex("^([a-z]|[a-z][-a-z0-9]*[a-z0-9])$", coalesce(var.prefix, "en"))) && length(coalesce(var.prefix, "en")) <= 16
+    condition = anytrue([
+      var.prefix == null,
+      alltrue([
+        can(regex("^([a-z]|[a-z][-a-z0-9]{0,14}[a-z0-9])$", var.prefix)),
+        length(regexall("^.*--.*", var.prefix)) == 0
+      ])
+    ])
+    error_message = "Prefix must begin with a lowercase letter, contain only lowercase letters, numbers, and - characters. Prefixes must end with a lowercase letter or number and be 16 or fewer characters."
   }
 }
 
