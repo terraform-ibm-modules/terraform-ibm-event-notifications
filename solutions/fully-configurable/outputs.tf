a500bd1f8089897c8d25b5c18633718f72a98aca
Md Anam Raihan <md.anam.raihan@ibm.com>
2025-06-10 11:14:58 +0530

    resolve comments

--- solutions/fully-configurable/outputs.tf
+++ solutions/fully-configurable/outputs.tf
@@ -7,9 +7,9 @@ output "event_notification_instance_name" {
   value       = var.existing_event_notifications_instance_crn == null ? module.event_notifications[0].event_notification_instance_name : data.ibm_resource_instance.existing_en_instance[0].name
 }
 
-output "en_instance_crn" {
+output "crn_list_object" {
   description = "A list of objects containing the CRN of the Event Notifications instance"
-  value       = local.use_existing_en_instance ? [{ en_crn = var.existing_event_notifications_instance_crn }] : [{ en_crn = module.event_notifications[0].crn }]
+  value       = local.use_existing_en_instance ? [{ crn = var.existing_event_notifications_instance_crn }] : [{ crn = module.event_notifications[0].crn }]
 }
 
 output "crn" {
