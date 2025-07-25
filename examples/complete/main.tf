be44821f44034dfbe35664f591bb5a690e482459
Terraform IBM Modules Operations <Terraform.IBM.Modules.Operations@ibm.com>
2025-07-25 23:25:22 +0000

    fix(deps): update terraform dependencies

--- examples/complete/main.tf
+++ examples/complete/main.tf
@@ -45,7 +45,7 @@ locals {
 
 module "cos" {
   source                 = "terraform-ibm-modules/cos/ibm"
-  version                = "10.1.12"
+  version                = "10.1.15"
   resource_group_id      = module.resource_group.resource_group_id
   region                 = var.region
   cos_instance_name      = "${var.prefix}-cos"
