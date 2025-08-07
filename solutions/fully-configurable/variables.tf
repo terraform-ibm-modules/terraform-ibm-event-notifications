d85cb8b3235d4bff33d80dcc7c042c0c22ba01c5
Md Anam Raihan <md.anam.raihan17@gmail.com>
2025-08-07 06:19:16 +0000

    updated variables.tf

--- solutions/fully-configurable/variables.tf
+++ solutions/fully-configurable/variables.tf
@@ -55,14 +55,14 @@ variable "prefix" {
 
 variable "event_notifications_access_tags" {
   type        = list(string)
-  description = "A list of access tags to apply to the Event Notifications instance created by the module. For more information, [see here](https://cloud.ibm.com/docs/account?topic=account-access-tags-tutorial)."
+  description = "A list of access tags to apply to the Event Notifications instance created by the solution. For more information, [see here](https://cloud.ibm.com/docs/account?topic=account-access-tags-tutorial)."
   default     = []
 
   validation {
     condition = alltrue([
       for tag in var.event_notifications_access_tags : can(regex("[\\w\\-_\\.]+:[\\w\\-_\\.]+", tag)) && length(tag) <= 128
     ])
-    error_message = "Tags must match the regular expression \"[\\w\\-_\\.]+:[\\w\\-_\\.]+\", [learn more](https://cloud.ibm.com/docs/account?topic=account-tag&interface=ui#limit) for more details."
+    error_message = "Tags must match the regular expression \"[\\w\\-_\\.]+:[\\w\\-_\\.]+\". For more information, [see here](https://cloud.ibm.com/docs/account?topic=account-tag&interface=ui#limit)."
   }
 }
 
