4da91b2c0c05b4ef491b494ddfca7d9ce7a3d052
Terraform IBM Modules Operations <106112202+terraform-ibm-modules-ops@users.noreply.github.com>
2025-08-23 08:43:11 +0530

    fix(deps): update dependencies (#537)

--- solutions/fully-configurable/main.tf
+++ solutions/fully-configurable/main.tf
@@ -236,7 +236,7 @@ locals {
 module "cos_buckets" {
   count          = var.enable_collecting_failed_events && var.existing_event_notifications_instance_crn == null ? 1 : 0
   source         = "terraform-ibm-modules/cos/ibm//modules/buckets"
-  version        = "10.2.4"
+  version        = "10.2.5"
   bucket_configs = local.bucket_config
 }
 
