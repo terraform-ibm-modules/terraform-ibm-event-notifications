c9c5c2c71c9ec6eb818a708765c7e4a711925b1d
Terraform IBM Modules Operations <Terraform.IBM.Modules.Operations@ibm.com>
2025-09-05 20:36:37 +0000

    fix(deps): update dependencies

--- examples/complete/main.tf
+++ examples/complete/main.tf
@@ -21,7 +21,7 @@ locals {
 
 module "key_protect_all_inclusive" {
   source                    = "terraform-ibm-modules/kms-all-inclusive/ibm"
-  version                   = "5.1.20"
+  version                   = "5.1.22"
   resource_group_id         = module.resource_group.resource_group_id
   region                    = var.region
   key_protect_instance_name = "${var.prefix}-kp"
@@ -45,7 +45,7 @@ locals {
 
 module "cos" {
   source                 = "terraform-ibm-modules/cos/ibm"
-  version                = "10.2.6"
+  version                = "10.2.13"
   resource_group_id      = module.resource_group.resource_group_id
   region                 = var.region
   cos_instance_name      = "${var.prefix}-cos"
