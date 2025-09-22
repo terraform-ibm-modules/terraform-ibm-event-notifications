487123f4f8c68b7534f5b8804ace6d93c0f2bd21
Arya Girish K <arya.girish.k@ibm.com>
2025-09-22 21:09:07 +0530

    feat: Add regex validation

--- solutions/security-enforced/variables.tf
+++ solutions/security-enforced/variables.tf
@@ -29,7 +29,7 @@ variable "existing_monitoring_crn" {
 
 variable "prefix" {
   type        = string
-  description = "The prefix to be added to all resources created by this solution. To skip using a prefix, set this value to null or an empty string. The prefix must begin with a lowercase letter and may contain only lowercase letters, digits, and hyphens '-'. It should not exceed 16 characters, must not end with a hyphen('-'), and can not contain consecutive hyphens ('--'). Example: en-0435. [Learn more](https://terraform-ibm-modules.github.io/documentation/#/prefix)."
+  description = "The prefix to add to all resources that this solution creates (e.g `prod`, `test`, `dev`). To skip using a prefix, set this value to null or an empty string. [Learn more](https://terraform-ibm-modules.github.io/documentation/#/prefix.md)."
   validation {
     condition = var.prefix == null || var.prefix == "" ? true : alltrue([
       can(regex("^[a-z][-a-z0-9]*[a-z0-9]$", var.prefix)), length(regexall("--", var.prefix)) == 0
