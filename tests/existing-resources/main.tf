adfa78d4f8c3d170a3ecc7dd7fd54479ff0e8d15
Terraform IBM Modules Operations <Terraform.IBM.Modules.Operations@ibm.com>
2025-10-26 04:59:03 +0000

    fix(deps): update terraform terraform-ibm-modules/cos/ibm to v10.5.2

--- tests/existing-resources/main.tf
+++ tests/existing-resources/main.tf
@@ -18,7 +18,7 @@ module "event_notification" {
 
 module "cos" {
   source                 = "terraform-ibm-modules/cos/ibm"
-  version                = "10.5.1"
+  version                = "10.5.2"
   resource_group_id      = module.resource_group.resource_group_id
   region                 = var.region
   cos_instance_name      = "${var.prefix}-cos"
