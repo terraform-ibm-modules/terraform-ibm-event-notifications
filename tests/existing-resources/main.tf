37a59429f0d5e082b94be82ce14b21f393aec918
Terraform IBM Modules Operations <106112202+terraform-ibm-modules-ops@users.noreply.github.com>
2025-12-13 15:59:13 +0000

    fix(deps): update dependencies (#599)

--- tests/existing-resources/main.tf
+++ tests/existing-resources/main.tf
@@ -1,6 +1,6 @@
 module "resource_group" {
   source                       = "terraform-ibm-modules/resource-group/ibm"
-  version                      = "1.4.3"
+  version                      = "1.4.5"
   resource_group_name          = var.resource_group == null ? "${var.prefix}-resource-group" : null
   existing_resource_group_name = var.resource_group
 }
@@ -18,7 +18,7 @@ module "event_notification" {
 
 module "cos" {
   source                 = "terraform-ibm-modules/cos/ibm"
-  version                = "10.7.0"
+  version                = "10.7.2"
   resource_group_id      = module.resource_group.resource_group_id
   region                 = var.region
   cos_instance_name      = "${var.prefix}-cos"
