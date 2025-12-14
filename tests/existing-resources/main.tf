8584d06cba34448e5dae5ebd3c59bbf0ed42fc4c
Terraform IBM Modules Operations <Terraform.IBM.Modules.Operations@ibm.com>
2025-12-14 05:05:09 +0000

    fix(deps): update dependencies

--- tests/existing-resources/main.tf
+++ tests/existing-resources/main.tf
@@ -39,7 +39,7 @@ module "cloud_monitoring" {
 
 module "kms_key" {
   source          = "terraform-ibm-modules/kms-key/ibm"
-  version         = "1.4.9"
+  version         = "1.4.11"
   kms_instance_id = "crn:v1:bluemix:public:hs-crypto:us-south:a/abac0df06b644a9cabc6e44f55b3880e:e6dce284-e80f-46e1-a3c1-830f7adff7a9::"
   key_name        = "${var.prefix}-root-key"
   force_delete    = true # Setting it to true for testing purpose
