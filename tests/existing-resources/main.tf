eab4f5a6b20761944d03efea73bd8735f2c49e78
Terraform IBM Modules Operations <106112202+terraform-ibm-modules-ops@users.noreply.github.com>
2025-12-21 02:28:45 +0000

    fix(deps): update dependencies (#604)

--- tests/existing-resources/main.tf
+++ tests/existing-resources/main.tf
@@ -18,7 +18,7 @@ module "event_notification" {
 
 module "cos" {
   source                 = "terraform-ibm-modules/cos/ibm"
-  version                = "10.7.3"
+  version                = "10.7.4"
   resource_group_id      = module.resource_group.resource_group_id
   region                 = var.region
   cos_instance_name      = "${var.prefix}-cos"
@@ -30,7 +30,7 @@ module "cos" {
 
 module "cloud_monitoring" {
   source            = "terraform-ibm-modules/cloud-monitoring/ibm"
-  version           = "1.12.2"
+  version           = "1.12.3"
   resource_group_id = module.resource_group.resource_group_id
   region            = var.region
   resource_tags     = var.resource_tags
@@ -39,7 +39,7 @@ module "cloud_monitoring" {
 
 module "kms_key" {
   source          = "terraform-ibm-modules/kms-key/ibm"
-  version         = "1.4.13"
+  version         = "1.4.14"
   kms_instance_id = "crn:v1:bluemix:public:hs-crypto:us-south:a/abac0df06b644a9cabc6e44f55b3880e:e6dce284-e80f-46e1-a3c1-830f7adff7a9::"
   key_name        = "${var.prefix}-root-key"
   force_delete    = true # Setting it to true for testing purpose
