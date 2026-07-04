870a4bc045dddf1768bb009352dfe58d2c8f5cd5
Terraform IBM Modules Operations <Terraform.IBM.Modules.Operations@ibm.com>
2026-07-04 04:43:23 +0000

    fix(deps): update dependencies

--- examples/fscloud/main.tf
+++ examples/fscloud/main.tf
@@ -23,7 +23,7 @@ data "ibm_iam_account_settings" "iam_account_settings" {
 
 module "cbr_zone_schematics" {
   source           = "terraform-ibm-modules/cbr/ibm//modules/cbr-zone-module"
-  version          = "1.36.6"
+  version          = "1.36.7"
   name             = "${var.prefix}-schematics-zone"
   zone_description = "CBR Network zone containing Schematics"
   account_id       = data.ibm_iam_account_settings.iam_account_settings.account_id
@@ -48,7 +48,7 @@ locals {
 
 module "cos" {
   source              = "terraform-ibm-modules/cos/ibm//modules/fscloud"
-  version             = "10.17.0"
+  version             = "10.17.1"
   resource_group_id   = module.resource_group.resource_group_id
   create_cos_instance = true
   cos_instance_name   = "${var.prefix}-cos"
